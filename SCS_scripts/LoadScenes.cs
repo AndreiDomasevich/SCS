f92f3c663829d3568013a6236c53c4a937e9260f
DimaVolynets450501 <dimanvolina@gmail.com>
2016-11-25 22:40:15 +0000

    add scenemanagment and reset scripts

---
+++ SCS_scripts/LoadScenes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour {
+
+	public void StartLevel() {
+		SceneManager.LoadScene ("SandDunesDemo");
+	}
+}
