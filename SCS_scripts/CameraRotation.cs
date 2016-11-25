334e67fbd47e3695ebe310743120abb3e68de9f4
DimaVolynets450501 <dimanvolina@gmail.com>
2016-11-25 22:33:31 +0000

    add camera scripts

---
+++ SCS_scripts/CameraRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	public Transform cameraTransform;
+	
+	void FixedUpdate (){ 
+		cameraTransform.localRotation = Quaternion.Euler ( 0.0f, Input.GetAxis ("Horizontal")* Time.deltaTime * 1000 , 0.0f);
+	}
+}
